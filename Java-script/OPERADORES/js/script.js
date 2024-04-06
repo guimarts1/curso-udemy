44ef78a4ff03d54eda7b1196908366e78c989eb3
guimarts1 <martins768ferreira@gmail.com>
2024-04-06 10:57:53 -0300

    curso js

---
+++ Java-script/OPERADORES/js/script.js
@@ -0,0 +1,11 @@
+// 1 - Number
+console.log(typeof 2)
+console.log(typeof 5.14)
+console.log(typeof -256)  
+
+// 2 - Operações Aritméticas
+console.log(12+13)
+console.log(23-6)
+console.log(2+(4*4))
+console.log(5*8)
+console.log(1500/2)
