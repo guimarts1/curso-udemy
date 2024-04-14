1ac3e7a9cdea673a2772ce0fe69d42d4e0ea4ef8
guimarts1 <martins768ferreira@gmail.com>
2024-04-14 20:43:15 -0300

    at js

--- Java-script/OPERADORES/js/script.js
+++ Java-script/OPERADORES/js/script.js
@@ -18,3 +18,61 @@ console.log(typeof -Infinity)
 console.log(12 * 'asd')
 
 console.log(typeof NaN)
+
+// 4 - Strings
+console.log("Texto")
+console.log('Texto')
+console.log(`Mais um texto`)
+
+console.log(typeof 'texto')
+console.log(typeof 'outro texto')
+
+// 5 Símbolos especiais em string
+console.log('Testando a \n quebra de linha')
+
+console.log('Espaçamento \t de tab')
+
+// 6 - Concatenação
+console.log('meu' + 'texto' + 'combinado')
+console.log("Oi, " + "tudo" +" bem?")
+
+console.log(`Testando` + ` com crase!`)
+
+// 7 - Interpolção - Template string
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log('teste')}`)
+
+// 8 - Booleans - true or false
+console.log(true)
+
+console.log(5 > 20)
+
+console.log(30 > 10)
+
+console.log(typeof true)
+
+// 9 - Comparações
+console.log(5 <= 5)
+
+console.log(4 < 4)
+
+console.log(10 == 10)
+
+console.log(10 == 9)
+
+console.log(10 != 9)
+
+// 10 - Comparação de idêntico
+console.log(9 == "9")
+
+console.log(9 + "9")
+
+console.log(9 === "9")
+
+// 11 - Operadores lógicos
+console.log()
+
+console.log()
+
+console.log()
