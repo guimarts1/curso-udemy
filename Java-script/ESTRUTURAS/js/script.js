2bff471a8bd49aff6f1080ad97e651fc89b471ac
guimarts1 <martins768ferreira@gmail.com>
2024-04-20 20:27:43 -0300

    curso js

--- Java-script/ESTRUTURAS/js/script.js
+++ Java-script/ESTRUTURAS/js/script.js
@@ -1,2 +1,12 @@
+// indentação muito importante!!
 
+// 14 - break
 
+for(let g = 20; g > 10; g--){
+    console.log(`o valor de g é ${g}`)
+
+    if(g === 15){
+        console.log("o g é 12!")
+        break;
+    }
+}
